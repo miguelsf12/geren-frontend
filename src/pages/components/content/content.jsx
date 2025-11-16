ed42790fb9f20dae12842409812ca3125a10913d
Miguel Santos <miguelsantosf12@gmail.com>
2025-11-16 16:05:57 -0300

    content criada

--- src/pages/components/content/content.jsx
+++ src/pages/components/content/content.jsx
@@ -1,7 +1,54 @@
+import contentStyles from "./content.module.css"
+
 export default function Content() {
   return (
-    <section className={}>
-
+    <section className={contentStyles.contentContainer}>
+      <div className={contentStyles.contentText}>
+        <h1>Tudo que você precisa em um só lugar</h1>
+        <p>
+          Recursos completos para facilitar sua rotina e otimizar o atendimento
+        </p>
+      </div>
+      <div className={contentStyles.cardsContainer}>
+        <div className={contentStyles.card}>
+          <div
+            className={`${contentStyles.iconeContainer} ${contentStyles.card1}`}
+          >
+            <i class="bi bi-calendar-event"></i>
+          </div>
+          <h2>Agenda Inteligente</h2>
+          <p>
+            Organize consultas, envie lembretes automáticos e reduza faltas.
+          </p>
+        </div>
+        <div className={contentStyles.card}>
+          <div
+            className={`${contentStyles.iconeContainer} ${contentStyles.card2}`}
+          >
+            <i class="bi bi-people"></i>
+          </div>
+          <h2>Gestão de Pacientes</h2>
+          <p>Histórico completo, prontuários digitais e dados sempre à mão.</p>
+        </div>
+        <div className={contentStyles.card}>
+          <div
+            className={`${contentStyles.iconeContainer} ${contentStyles.card3}`}
+          >
+            <i class="bi bi-currency-dollar"></i>
+          </div>
+          <h2>Controle Financeiro</h2>
+          <p>Acompanhe receitas, despesas e tenha relatórios detalhados.</p>
+        </div>
+        <div className={contentStyles.card}>
+          <div
+            className={`${contentStyles.iconeContainer} ${contentStyles.card4}`}
+          >
+            <i class="bi bi-bar-chart-line-fill"></i>
+          </div>
+          <h2>Relatórios e Análises</h2>
+          <p>Visualize o desempenho do seu consultório com dados claros.</p>
+        </div>
+      </div>
     </section>
   )
 }
